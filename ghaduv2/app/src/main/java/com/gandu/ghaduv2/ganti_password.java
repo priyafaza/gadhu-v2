cae99b93cf31887d346c49676bf23fdd61aa9774
priyafaza <38573986+priyafaza@users.noreply.github.com>
2018-11-14 15:05:41 +0700

    Add files via upload

---
+++ ghaduv2/app/src/main/java/com/gandu/ghaduv2/ganti_password.java
@@ -0,0 +1,14 @@
+package com.gandu.ghaduv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import com.gandu.ghaduv2.ganti_password;
+
+public class ganti_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganti_password);
+    }
+}
